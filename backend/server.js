095baf7bc875cf01dbf4c91db1cb48875d12f416
Paskool87 <pascalfebury@yahoo.fr>
2026-09-23 10:12:55 +0200

    Ajout de la première route API

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,12 @@ const app = express();
 
 const PORT = 3000;
 
+app.get("/api", (req, res) => {
+    res.json({
+        message: "API DeckDuel opérationnelle"
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Serveur DeckDuel démarré sur le port ${PORT}`);
 });
